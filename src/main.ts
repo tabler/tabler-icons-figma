444c876fae8e40d3e7f513f718108fafdfb39323
codecalm <codecalm@gmail.com>
2023-05-18 10:07:42 +0200

    add possibility to paste icons as outline stroke

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,31 @@
-import { on, showUI } from '@create-figma-plugin/utilities'
+import { on, showUI } from "@create-figma-plugin/utilities";
 
 export default function () {
-	showUI({
-		width: 300,
-		height: 400
-	})
+  showUI({
+    width: 300,
+    height: 400,
+  });
 
-	function handleSubmit(data: { name: string, svg: string }) {
-		const icon = figma.createNodeFromSvg(data.svg)
+  function handleSubmit(data: { name: string; svg: string, outlineStroke: boolean }) {
+    const icon = figma.createNodeFromSvg(data.svg);
 
-		icon.name = `tabler-icon-${data.name}`
-		icon.x = Math.round(figma.viewport.center.x)
-		icon.y = Math.round(figma.viewport.center.y)
+    icon.name = `tabler-icon-${data.name}`;
+    icon.x = Math.round(figma.viewport.center.x);
+    icon.y = Math.round(figma.viewport.center.y);
 
-		figma.currentPage.selection = [icon]
-	}
+    const flattened = figma.flatten(icon.children, icon);
 
-	on('SUBMIT', handleSubmit)
+    if (data.outlineStroke) {
+      const stroke = flattened.outlineStroke();
+
+      if (stroke) {
+        flattened.remove();
+        icon.appendChild(stroke);
+      }
+    }
+
+    figma.currentPage.selection = [icon];
+  }
+
+  on("SUBMIT", handleSubmit);
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,18 +1,20 @@
 import '!./ui.css'
 
 import {
-	Container,
-	Divider,
-	Bold,
-	render,
-	Text,
-	Muted,
-	VerticalSpace,
-	SearchTextbox,
-	DropdownOption,
-	Dropdown,
-	Columns
-} from '@create-figma-plugin/ui'
+  Container,
+  Divider,
+  Bold,
+  render,
+  Text,
+  Muted,
+  VerticalSpace,
+  SearchTextbox,
+  DropdownOption,
+  Dropdown,
+  Columns,
+  Checkbox,
+  Link,
+} from "@create-figma-plugin/ui";
 import {
 	emit,
 } from '@create-figma-plugin/utilities'
@@ -30,38 +32,40 @@ type Icon = {
 }
 
 function IconButton({
-	icon,
-	stroke
+  icon,
+  stroke,
+  outlineStroke,
 }: {
-	icon: Icon,
-	stroke: string
+  icon: Icon;
+  stroke: string;
+  outlineStroke: boolean;
 }) {
-	const svg = icon.svg
-		.replace('stroke-width="2"', `stroke-width="${stroke}"`)
+  const svg = icon.svg.replace('stroke-width="2"', `stroke-width="${stroke}"`);
 
-	const handleClick = (name: string, svg: string) => {
-		emit('SUBMIT', {
-			name,
-			svg
-		})
-	}
+  const handleClick = (name: string, svg: string) => {
+    emit("SUBMIT", {
+      name,
+      svg,
+      outlineStroke,
+    });
+  };
 
-	return (
-		<button
-			key={icon.name}
-			aria-label={icon.name}
-			onClick={() => handleClick(icon.name, svg)}
-			class="icon-button"
-			dangerouslySetInnerHTML={{ __html: svg }}
-		>
-		</button>
-	)
+  return (
+    <button
+      key={icon.name}
+      aria-label={icon.name}
+      onClick={() => handleClick(icon.name, svg)}
+      class="icon-button"
+      dangerouslySetInnerHTML={{ __html: svg }}
+    ></button>
+  );
 }
 
 function Plugin() {
 	const [search, setSearch] = useState<string>('')
 	const [category, setCategory] = useState<string>('')
 	const [stroke, setStroke] = useState<string>('2')
+	const [outlineStroke, setOutlineStroke] = useState<boolean>(false);
 
 	const results = useSearch(search, category)
 	const limit = 102
@@ -77,6 +81,10 @@ function Plugin() {
 	function handleStrokeChange(event: JSX.TargetedEvent<HTMLInputElement>) {
 		setStroke(event.currentTarget.value)
 	}
+
+	function handleOutlineChange(event: JSX.TargetedEvent<HTMLInputElement>) {
+    setOutlineStroke(event.currentTarget.checked);
+  }
 
 	let c: string[] = []
 	icons.forEach((i) => {
@@ -105,71 +113,106 @@ function Plugin() {
 	]
 
 	return (
-		<div>
-			<div class="search">
-				<SearchTextbox
-					onInput={handleInput}
-					placeholder={`Search ${icons.length} icons`}
-					value={search}
-				/>
-				<Divider />
-				<Container space="extraSmall">
-					<VerticalSpace space="extraSmall" />
-					<Columns space="small">
-						<Dropdown onChange={handleCategoryChange} options={categories} value={category} />
-						<Dropdown onChange={handleStrokeChange} options={strokes} value={stroke} />
-					</Columns>
-					<VerticalSpace space="extraSmall" />
-				</Container>
-				<Divider />
-			</div>
-			<Container space="small">
-				<VerticalSpace space="small" />
-				{(search || category != '') && (
-					<div>
-						<Text>
-							<Bold>
-								Icons
-								{search && ` matched "${search}"`}
-								{category != '' && ` in category "${category}"`}
-								{":"}
-							</Bold>
-						</Text>
-						<VerticalSpace space="small" />
-					</div>
-				)}
-			</Container>
-			<Container space="small">
-				<div class="grid">
-					{results.slice(0, limit).map(icon => (
-						<IconButton icon={icon} stroke={stroke}/>
-					))}
-				</div>
-				{results.length === 0 && (
-					<div>
-						<VerticalSpace space="medium" />
-						<Text align="center">
-							<Muted>Sorry, we don't have any icon to match your query.</Muted>
-						</Text>
-						<VerticalSpace space="large" />
-					</div>
-				)}
-				{results.length - limit > 0 && (
-					<div>
-						<VerticalSpace space="medium" />
-						<Text align="center">
-						<Muted>&hellip;and {results.length - limit} more. Use the search to find more icons.</Muted>
-						</Text>
-					</div>
-				)}
-				<VerticalSpace space="extraLarge" />
-				<Text>
-					<Muted>Tabler Icons v{version}</Muted>
-				</Text>
-				<VerticalSpace space="medium" />
-			</Container>
-		</div>
-	)
+    <div>
+      <div class="search">
+        <SearchTextbox
+          onInput={handleInput}
+          placeholder={`Search ${icons.length} icons`}
+          value={search}
+        />
+        <Divider />
+        <Container space="extraSmall">
+          <VerticalSpace space="extraSmall" />
+          <Columns space="small">
+            <Dropdown
+              onChange={handleCategoryChange}
+              options={categories}
+              value={category}
+            />
+            <Dropdown
+              onChange={handleStrokeChange}
+              options={strokes}
+              value={stroke}
+            />
+          </Columns>
+          <VerticalSpace space="extraSmall" />
+        </Container>
+        <Divider />
+      </div>
+      <Container space="small">
+        <VerticalSpace space="small" />
+        {(search || category != "") && (
+          <div>
+            <Text>
+              <Bold>
+                Icons
+                {search && ` matched "${search}"`}
+                {category != "" && ` in category "${category}"`}
+                {":"}
+              </Bold>
+            </Text>
+            <VerticalSpace space="small" />
+          </div>
+        )}
+      </Container>
+      <Container space="small">
+        <div class="grid">
+          {results.slice(0, limit).map((icon) => (
+            <IconButton
+              icon={icon}
+              stroke={stroke}
+              outlineStroke={outlineStroke}
+            />
+          ))}
+        </div>
+        {results.length === 0 && (
+          <div>
+            <VerticalSpace space="medium" />
+            <Text align="center">
+              <Muted>Sorry, we don't have any icon to match your query.</Muted>
+            </Text>
+            <VerticalSpace space="large" />
+          </div>
+        )}
+        {results.length - limit > 0 && (
+          <div>
+            <VerticalSpace space="medium" />
+            <Text align="center">
+              <Muted>
+                &hellip;and {results.length - limit} more. Use the search to
+                find more icons.
+              </Muted>
+            </Text>
+          </div>
+        )}
+        <VerticalSpace space="extraLarge" />
+        <Text>
+          <Muted>Tabler Icons v{version}</Muted>
+        </Text>
+        <VerticalSpace space="medium" />
+      </Container>
+      <div className="footer">
+        <Divider />
+        <Container space="medium">
+          <VerticalSpace space="small" />
+          <Columns style={{ alignItems: "center" }}>
+            <Checkbox onChange={handleOutlineChange} value={outlineStroke}>
+              <Text>Paste icon as outline</Text>
+            </Checkbox>
+            <Text align="right">
+              <Link
+                href="https://tabler-icons.io/?utm_source=figma-plugin"
+                target="_blank"
+              >
+                Tabler Icons
+              </Link>
+            </Text>
+          </Columns>
+          <VerticalSpace space="small" />
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default render(Plugin)
